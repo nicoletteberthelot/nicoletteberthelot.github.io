05aff2bcc06d4a628c74aae8763e1a792569fa6a
nicoletteb <littlemissnico@gmail.com>
2018-05-05 09:23:22 -0400

    Final Project Pseudocode

---
+++ homework/Final_Project/js/final.js
@@ -0,0 +1,9 @@
+//As a user, I want to be able to navigate around the NYC subway in my wheelchair without having to change routes based on broekn elevators that I was not aware of.
+
+// As a user, I want to be able to enter a starting address and destination address in the input field 
+
+// As a user, I want to be able to see a recommended journey on a subway line to my destination
+
+// As a user, I want to be able to see what stations along my journey have working elevators
+
+// (If this gets too complicated due to different factors such as multiple train lines on a journey etc)...As a user, I want to be able to enter my specific train line in an input field and see what stations on the line have working elevators.
---
+++ homework/google_maps_example/js/app.js
@@ -0,0 +1,53 @@
+// As a User
+// When I view the site
+// I see a map
+   // var map;
+   //    function initMap() {
+   //      map = new google.maps.Map(document.getElementById('map'), {
+   //        center: {lat: -34.397, lng: 150.644},
+   //        zoom: 8
+   //      });
+   //    }
+
+// As a User
+// When I view the site
+// I expect to only see NYC
+
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.730610, lng: -73.935242},
+          zoom: 11
+        });
+        var newSpot lat: 40.730610, lng: -73.935242}
+        var marker = new google.maps.Marker({
+          position: newSpot,
+          map: map
+        });
+      }
+
+
+
+// As a User
+// When I view the site
+// I want to see a marker on NYC
+
+// Bonus:
+
+// As a User
+// When I view the map
+// I want to see the terrain view
+
+// More Bonus:
+
+// As a User
+// When I press a button
+// I see 3 markers appear on the map
+
+// Unbelievable, Seriously???? Bonus
+
+// As a User
+// When I view the site
+// All the CitiBike NYC stations appear on the map as markers
+
+// Hint: Make sure to use this URL: https://api.citybik.es/v2/networks/citi-bike-nyc
