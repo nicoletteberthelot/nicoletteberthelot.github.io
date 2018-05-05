814331979714ba51f548e7cb8879995d41bb6919
nicoletteb <littlemissnico@gmail.com>
2018-05-05 09:44:59 -0400

    Final Project Pseudocode

--- homework/Final_Project/js/final.js
+++ homework/Final_Project/js/final.js
@@ -1,4 +1,4 @@
-// As a user, I want to be able to navigate around the NYC subway efficiently without having to change routes due to broken elevators that I was not aware of so I can get to my destination on time if I am disabled.
+// As a user, I want to be able to navigate around the NYC subway efficiently without having to change routes due to broken elevators so I can get to my destination on time if I am disabled.
 
 // As a user, I want to be able to enter a starting address and destination address in the input field 
 
@@ -6,4 +6,4 @@
 
 // As a user, I want to be able to see what stations along my journey have working elevators
 
-// (If this gets too complicated due to different factors such as multiple train lines on a journey etc)...As a user, I want to be able to enter my specific train line in an input field and see what stations on the line have working elevators.
+// (If this gets too complicated due to different factors such as multiple train lines on a journey etc)...As a user, I want to be able to enter my specific train line in an input field and see what stations on that line have working elevators.
