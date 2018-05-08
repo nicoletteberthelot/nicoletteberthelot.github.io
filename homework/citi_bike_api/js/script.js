b251a367bb81c310a55277274fd306ce73f0a9f6
nicoletteb <littlemissnico@gmail.com>
2018-05-07 22:44:43 -0400

    Citi Bikes Homework

--- homework/citi_bike_api/js/script.js
+++ homework/citi_bike_api/js/script.js
@@ -1,13 +1,38 @@
 // As a User
 // When I visit the site
 // I expect to see the names of the all the stations
-// var array = ["0 … 99", "100 … 199", "200 … 299", "300 … 399", "400 … 499", "500 … 599", "600 … 699", "700 … 799", "800 … 810"]
 
+$.ajax({
+  type: 'GET',
+  url: 'http://api.citybik.es/citi-bike-nyc.json',
+  success: function(stations) {
+  	stations.filter(bikes  {
+	console.log(element);
+});
+  }
+});
 
+console.log(result);
 
 // As a User
 // When I visit the site
 // I expect to see only the first fifty stations on the list
+
+$.ajax(
+{
+  type: 'GET',
+  url: 'http://api.citybik.es/citi-bike-nyc.json',
+  success: function(ajaxResponse) {
+  	const shortList = ajaxResponse.filter(item => item.idx <= 50);
+  		ajaxResponse.forEach(
+  		function(ajaxResponse) {	
+  		var output = "<li>" + ajaxResponse.name + "</li>";
+  		$("#list").append(output);
+	}
+	)
+	}
+}
+)
 
 // As a User
 // When I visit the site
@@ -25,12 +50,6 @@
 // When I select "number of free bikes" from a dropdown
 // I see fifty stations where the "number of free bikes" is greater than my selection
 
-$.ajax({
-  type: 'GET',
-  url: 'http://api.citybik.es/citi-bike-nyc.json',
-  success: function(stations) {
-  	stations.forEach(function(element) {
-	console.log(element);
-});
-  }
-});
+	
+
+
